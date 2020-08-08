725d57285a755f03791b705ddb97f799ff7956ed
Andrey Sokolov <artas90@gmail.com>
2020-08-08 14:40:04 +0300

    initial commit

---
+++ src/bundle.mjs
@@ -0,0 +1,35 @@
+import { 
+  h,
+  Component,
+  createContext,
+  render
+} from 'preact';
+import {
+  useState, useReducer, useEffect, useLayoutEffect, useRef,
+  useImperativeHandle, useMemo, useCallback, useContext, useDebugValue
+} from 'preact/hooks';
+import htm from 'htm';
+import linkState from 'linkstate';
+import mitt from 'mitt';
+
+const html = htm.bind(h);
+
+export { 
+  h,
+  html,
+  render,
+  Component,
+  createContext,
+  useState,
+  useReducer,
+  useEffect,
+  useLayoutEffect,
+  useRef,
+  useImperativeHandle,
+  useMemo,
+  useCallback,
+  useContext,
+  useDebugValue,
+  linkState,
+  mitt,
+};
